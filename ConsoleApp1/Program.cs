2d4467c22185ccbd04127de3640f20c75e40eec9
Nader Zaky <nader.mohammed.zaki@gmail.com>
2020-04-30 01:50:28 +0200

    last changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -573,6 +573,21 @@ namespace ConsoleApp1
             return chars.Length;
         }
 
+        public static int removeDuplicates(int[] nums)  // remove duplicates from sorted array "In Place"
+        {
+            if (nums.Length == 0) return 0;
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j] != nums[i])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
+
     }
    
       
